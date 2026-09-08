f1841d3980e5c342651a2d3552682428436a24a6
Zehra <zahranaqvi@live.com>
2026-09-08 10:39:56 +0200

    report added

--- Specs/jest.config.js
+++ Specs/jest.config.js
@@ -1,5 +1,19 @@
-//Dec: jest configuration file 
-module.exports =
-{
-    testMatch:['**/Specs/*.spec.js'],
+// Desc: Jest configuration file
+module.exports = {
+    // specify the location of the test file
+    testMatch: ['**/Specs/*.spec.js'],
+    // test report generator
+    reporters: [
+        'default',
+        [
+            'jest-html-reporters',
+            {
+                publicPath: './Report', // report directory
+                filename: 'report.html', // report file name
+                pageTitle: 'SuperTest and Jest API Test Report', // report title
+                overwrite: true, // enable report file overwrite
+                expand: true, // enable report file expansion
+            },
+        ],
+    ],
 };
